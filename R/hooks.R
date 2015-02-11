bd7676c8cf7d1560aa97744c379322421c13484e
kos59125 <mail@recyclebin.jp>
2015-02-12 02:36:27 +0900

    Avoid using .onLoad according to CHECK program

--- R/hooks.R
+++ R/hooks.R
@@ -1,4 +1,4 @@
 #' @export
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
    packageStartupMessage("Run options(error = DYM) to enable 'Did you mean' feature", domain="R-DYM")
 }
