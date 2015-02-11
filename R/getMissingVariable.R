71cd9bf4fed40a06341f345576c0e74f76a630ac
kos59125 <mail@recyclebin.jp>
2015-02-12 02:23:35 +0900

    Suggest when function is not found

--- R/getMissingVariable.R
+++ R/getMissingVariable.R
@@ -5,10 +5,17 @@
 #' and returns the name of the mistype if it is found.
 getMissingVariable <- function() {
    errorMessage <- geterrmessage()
-   notFound <- ngettext(1L, "object '%s' not found", "", domain="R")
-   pattern <- sub("'%s'", "'([^']+)'", sprintf("^.*%s.*$", notFound))
-   if (grepl(pattern, errorMessage)) {
-      sub(pattern, "\\1", errorMessage)
+   
+   functionNotFound <- ngettext(1L, "could not find function \"%s\"", "", domain="R")
+   functionNotFoundPattern <- sub("\"%s\"", "\"([^\"]+)\"", sprintf("^.*%s.*$", functionNotFound))
+
+   objectNotFound <- ngettext(1L, "object '%s' not found", "", domain="R")
+   objectNotFoundPattern <- sub("'%s'", "'([^']+)'", sprintf("^.*%s.*$", objectNotFound))
+   
+   if (grepl(functionNotFoundPattern, errorMessage)) {
+      sub(functionNotFoundPattern, "\\1", errorMessage)
+   } else if (grepl(objectNotFoundPattern, errorMessage)) {
+      sub(objectNotFoundPattern, "\\1", errorMessage)
    } else {
       NA_character_
    }
