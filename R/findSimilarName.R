010b85821b7c2f3b12b45a96c470faca2fed04bd
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-01-18 11:28:31 +0300

    Better algorithm for findSimilarNames.  Fixes issue 3. https://github.com/kos59125/DYM/issues/3

--- R/findSimilarName.R
+++ R/findSimilarName.R
@@ -2,19 +2,42 @@
 #' within \code{name} (the second) argument.
 #' 
 #' @param x
-#'    The name to search
+#'    A string giving the (misspelt) name to search for.
 #' @param names
-#'    The candidates of the correct.
+#'    A character vector of correct names to match against.
 #' @param threshold
 #'    The maximum distance between the misspell (\code{x}) and the correct answer (in \code{name}).
-#' 
-#' @seealso agrep
-findSimilarName <- function(x, names, threshold=2) {
+#' @param max_n
+#'    An integer limiting the number of results.  Passed to \code{\link[utils]{head}}.
+#' @seealso \code{\link[utils]{adist}} calculates the distance between strings.
+#' \code{\link[base]{agrep}} and \code{\link[stringdist]{stringdist-package}} 
+#' provide alternate metrics for these distances.
+#' @examples
+#' \donttest{
+#' x <- "logg"
+#' names <- DYM:::getNames(x)
+#' lapply(
+#'   stats::setNames(0:4, 0:4), 
+#'   function(i) DYM:::findSimilarName(x, names, threshold = i)
+#' )
+#' }
+#' @importFrom stats na.omit
+#' @importFrom utils adist
+#' @importFrom utils head
+findSimilarName <- function (x, names, threshold = 2, max_n = 10) 
+{
    if (is.na(x)) {
       character(0)
-   } else {
+   }
+   else {
       names <- na.omit(names)
-      d <- adist(x, names)
-      names[d == min(d) & d <= threshold]
+      d <- as.integer(adist(x, names))
+      distance_data <- data.frame(
+         names = names, 
+         distance = d,
+         stringsAsFactors = FALSE
+      )[order(d), ]
+      distance_data <- distance_data[distance_data$distance <= threshold, ]
+      head(distance_data, max_n)$names
    }
 }
