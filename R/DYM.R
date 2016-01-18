cbe3640dc47a0bd0daf4a988a81f642f5dd2f376
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-01-18 11:31:01 +0300

    theshold, max_n passed to findSimilarNames

--- R/DYM.R
+++ R/DYM.R
@@ -10,8 +10,17 @@ NULL
 #' If the function is called after an error of 'object not found',
 #' the function tries to tell you the name of the correct name that you meant.
 #' 
+#' @param threshold
+#'    The maximum distance between the misspell (\code{x}) and the correct answer (in \code{name}).
+#' @param max_n
+#'    An integer limiting the number of results.  Passed to \code{\link[utils]{head}}.
+#' @examples 
+#' \dontrun{
+#' options(error = DYM::DYM)
+#' logg(10)
+#' }
 #' @export
-DYM <- function() {
+DYM <- function(threshold = 2, max_n = 10) {
    missingVariable <- getMissingVariable()
    if (!is.na(missingVariable)) {
       envir <- attr(missingVariable, "package")
@@ -19,7 +28,7 @@ DYM <- function() {
          envir <- .GlobalEnv
       }
       availableVariables <- getNames(class(missingVariable), envir)
-      names <- findSimilarName(missingVariable, availableVariables)
+      names <- findSimilarName(missingVariable, availableVariables, threshold, max_n)
       if (length(names) > 0L) {
          message <- ngettext(length(names), "Did you mean: %s", "Did you mean: [%s]", domain="R-DYM")
          hints <- sapply(names, sprintf, fmt="'%s'")  # sQuote might be better
